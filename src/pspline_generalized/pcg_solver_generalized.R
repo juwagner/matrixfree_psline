a1ed1c531e650df515ac0a2d91cdd02feb3079cc
juwagner <juwagner1989@gmail.com>
2026-07-03 08:43:24 +0200

    Add weighted preconditioner to generalized PCG

--- src/pspline_generalized/pcg_solver_generalized.R
+++ src/pspline_generalized/pcg_solver_generalized.R
@@ -2,45 +2,15 @@
 # Diagonal-preconditioned Conjugate Gradient (PCG) solver for generalized P-splines
 # ------------------------------------------------------------------------------
 
+source("src/pspline/pcg_solver.R")
 source("src/pspline_generalized/pspline_operations_generalized.R")
 
 # ------------------------------------------------------------------------------
-# Compute diag(Λ) where Λ = sum_p (I ⊗ L_p ⊗ I)
-# L_list = List of penalty matrices L_p (each J_p × J_p)
-get_diag_Lambda <- function(L_list) {
-  P <- length(L_list)
-  J_vec <- vapply(L_list, function(L_p) ncol(L_p), integer(1))
-  K <- prod(J_vec)
-  
-  if (P == 1L) {
-    d <- as.vector(diag(L_list[[1L]]))
-    return(d)
-  }
-  
-  cp_fwd  <- cumprod(J_vec)
-  n_left  <- c(1L, cp_fwd[-P])
-  
-  cp_bwd  <- cumprod(rev(J_vec))
-  n_right <- c(rev(cp_bwd)[-1L], 1L)
-  
-  diag_pen <- numeric(K)
-  for (p in seq_len(P)) {
-    dp <- diag(L_list[[p]])
-    block <- rep(rep(dp, each = n_right[p]), times = n_left[p])
-    diag_pen <- diag_pen + block
-  }
-  return(as.vector(diag_pen))
-}
-
-# ------------------------------------------------------------------------------
-# Compute diag(ΦᵀΦ) matrix-free
+# Compute diag(Φᵀ W Φ) matrix-free, W a per-observation weight vector
 # PhiT_list List of transposed marginal B-spline bases Φ_pᵀ.
-get_diag_PhiTPhi <- function(PhiT_list) {
-  diag_gram_khatrirao(A_list = PhiT_list)
+get_diag_PhiTWPhi <- function(PhiT_list, W) {
+  diag_gram_khatrirao_weighted(A_list = PhiT_list, W = W)
 }
-
-# ------------------------------------------------------------------------------
-# ToDo: compute diag(Φᵀ W Φ) as preconditioner
 
 # ------------------------------------------------------------------------------
 # Solve (Φᵀ W Φ + λΛ) α = b using diagonal-preconditioned CG
@@ -60,9 +30,9 @@ solve_pcg_generalized = function(
   J_vec <- sapply(1:P, function(p) dim(L_list[[p]])[2] )
   K <- prod(J_vec)
   
-  diag_PhiTPhi <- get_diag_PhiTPhi(PhiT_list = PhiT_list)
+  diag_PhiTWPhi <- get_diag_PhiTWPhi(PhiT_list = PhiT_list, W = W)
   diag_Lambda <- get_diag_Lambda(L_list = L_list)
-  preconditioner <- 1 / (diag_PhiTPhi + lambda*diag_Lambda)
+  preconditioner <- 1 / (diag_PhiTWPhi + lambda*diag_Lambda)
   
   norm_b <- sqrt(drop(crossprod(b)))
   
